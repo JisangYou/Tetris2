1a63a5d67a1714ee2ec7aad2e59ffdfdd571d927
Jisang you <jisangman89@gmail.com>
2017-10-12 00:01:36 +0900

    first commit

---
+++ src/main/java/orgs/androidtown/tetris2/Dummy.java
@@ -0,0 +1,8 @@
+package orgs.androidtown.tetris2;
+
+/**
+ * Created by Jisang on 2017-09-29.
+ */
+
+public class Dummy {
+}
---
+++ src/main/java/orgs/androidtown/tetris2/Property.java
@@ -0,0 +1,14 @@
+package orgs.androidtown.tetris2;
+
+import android.graphics.Paint;
+
+/**
+ * Created by Jisang on 2017-09-29.
+ */
+
+public class Property {
+    float unit; // 크기 단위
+    float x;
+    float y;
+    Paint paint;
+}
